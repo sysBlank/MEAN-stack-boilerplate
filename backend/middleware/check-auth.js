c6c2940f2d9cdf2bfa0f4c71c9c56a06e80e62e5
Alexandru <a.ungureanu@oltrematica.it>
2023-04-08 17:09:07 +0200

    Token reset on request

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,11 +4,11 @@ const defaultError = require('../helpers/customErrors');
 module.exports = (req, res, next) => {
   try {
     const token = req.cookies['access_token'];
+    const token_time = 3600; // set token to expire in 1h
     if (!token) {
       throw defaultError('No token provided', 401, 'auth_failed')
     }
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.userData = decoded;
     // Reset the token expiration time
     const newToken = jwt.sign(
       {
@@ -22,7 +22,7 @@ module.exports = (req, res, next) => {
         expiresIn: "1h"
       }
     );
-    res.setHeader('token', newToken);
+    res.cookie("access_token", newToken, { maxAge: token_time * 1000, httpOnly: true });
     next();
   } catch (error) {
     return res.status(401).json({
